5be8c8dba7ffe9b45179ff917c4f25ef2009be49
JU <2570654086@qq.com>
2026-01-08 19:14:42 +0800

    确保所有Rust代码和配置都提交了

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,13 +4,11 @@ mod commands;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            commands::proxy_http_request,
-            commands::upload_file
-        ])
+        // 👇👇👇 必须加上这一行！这就是缺失的“点火器” 👇👇👇
+        .plugin(tauri_plugin_updater::Builder::new().build()) 
+        // 👆👆👆 没有这一行，JSON 里的配置全是废纸 👆👆👆
+        
+        .plugin(tauri_plugin_shell::init()) // 你原有的 shell 插件
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
